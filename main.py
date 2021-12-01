9498784dad328b768d8c5aa68135aaa33955ef12
Karyamsetty Martin <ksnabielmartin.mec18@itbhu.ac.in>
2021-12-01 18:23:26 +0530

    'gojek'

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+class user:
+    def __init__(self, name, uid,dollar=0,cents=0,max_balance=10**9):
+        self.name = name
+        self.uid = uid
+        self.balance = {"d": dollar, "m": max_balance,"c":cents}
+
+    def __str__(self):
+        return f"{self.name} is {self.uid}"
+    
+    def deposit(self, amount):
+        print(f"{self.name} is depositing {amount}")
+
+    def withdraw(self, amount):
+        print(f"{self.name} is withdrawing {amount}")
+    
+    def getBalance(self):
+        print(f"{self.name}'s balance is {self.balance}")
+
+    def __del__(self):
+        print("User object destroyed")
+
 def parse():
     print("$ Enter an amount")
     s = input("=>")
@@ -38,49 +59,81 @@ def parse():
         c = c%100
     return c,d
 
-def credit(balance):
-    c,d = parse()
+def credit(customer,amount=None):
+    if not amount:
+        c,d = parse()
+    else:
+        c,d = amount
     if not c and not d:
         return
-    tmpd = balance["d"]+d
-    tmpc = balance["c"]+c
-    if tmpc>balance["m"] or tmpd>balance["m"]:
-        print("$ Max amount is " + str(balance["m"]) + "D" )
+    tmpd = customer.balance["d"]+d
+    tmpc = customer.balance["c"]+c
+    if tmpc < 0:
+        tmpd-=1
+        tmpc+=100
+    if tmpc>customer.balance["m"] or tmpd>customer.balance["m"]:
+        print("$ Max amount is " + str(customer.balance["m"]) + "D" )
         return
-    balance["d"]=tmpd
-    balance["c"]=tmpc
+    customer.balance["d"]=tmpd
+    customer.balance["c"]=tmpc
     print("$ Done")
 
-def debit(balance):
-    c,d=parse()
+def debit(customer,amount=None):
+    if not amount:
+        c,d = parse()
+    else:
+        c,d = amount
     if not c and not d:
         return
-    balance["d"]-=d
-    balance["c"]-=c
+    tmpd = customer.balance["d"]-d
+    tmpc = customer.balance["c"]-c
+    if tmpc < 0:
+        tmpd-=1
+        tmpc+=100
+    if tmpc>customer.balance["m"] or tmpd>customer.balance["m"]:
+        print("$ Max amount is " + str(customer.balance["m"]) + "D" )
+        return
+    customer.balance["d"]=tmpd
+    customer.balance["c"]=tmpc
     print("$ Done")
 
-def check(balance):
-    print("$ Current balance is "+str(balance["d"])+"D "+str(balance["c"])+"C")
+def check(customer):
+    customer.getBalance()
 
-def main():
-    balance = {"d":0,"c":0,"m":10**9}
+def transfer(sender,receiver,amount):
+    debit(sender,amount)
+    credit(receiver,amount)
+
+
+def run():
+    bank =[user("John",1),user("Jane",2),user("Jack",3)]
     while True:
         try:
+            uid = int(input("$ Enter your uid: "))
+            u = bank[uid-1]
             print("""
 $ Select an option:
 1. Credit
 2. Debit
 3. Check
-4. Exit
+4. Transfer
+5. Exit
             """)
             choice = int(input("=>"))
             if choice == 1:
-                credit(balance)
+                credit(u)
             elif choice == 2:
-                debit(balance)
+                debit(u)
             elif choice == 3:
-                check(balance)
+                check(u)
             elif choice == 4:
+                reciver_uid = int(input("$ Enter receiver's UID: "))
+                reciver = bank[reciver_uid-1]
+                amount = parse()
+                if amount == (None,None):
+                    continue
+                transfer(u,reciver,amount)
+            elif choice == 5:
                 print("$ Thank you!")
                 break
             else:
@@ -89,4 +142,6 @@ $ Select an option:
             print("Please enter valid input :" )
 
 
-main()
+if __name__ == "__main__":
+    run()
+
